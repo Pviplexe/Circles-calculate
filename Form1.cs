08d4e93c9c9709b868ef8b7f31a07e0ac4a550dd
Pviplexe <128280471+Pviplexe@users.noreply.github.com>
2025-08-28 20:42:49 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿namespace W2work1_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCir1_Click(object sender, EventArgs e)
+        {
+            double r = Convert.ToDouble(txtRadius.Text);
+            double Cir = 2 * Math.PI * r;
+            string ans = "เส้นรอบวง = " + Cir;
+            txtAns.Text = ans;
+        }
+
+        private void btnCir2_Click(object sender, EventArgs e)
+        {
+            double r = Convert.ToDouble(txtRadius.Text);
+            double area = Math.PI *Math.Pow(r,2);
+            string ans = "เส้นรอบวง = " + area;
+            txtAns.Text = ans;
+        }
+
+        private void btnCir3_Click(object sender, EventArgs e)
+        {
+            double r = Convert.ToDouble(txtRadius.Text);
+            double sur = 4 *Math.PI*Math.Pow(r,2);
+            string ans = "เส้นรอบวง = " + sur;
+            txtAns.Text = ans;
+        }
+
+        private void btnCir4_Click(object sender, EventArgs e)
+        {
+            double r = Convert.ToDouble(txtRadius.Text);
+            double vol = (4.0/3.0)*Math.PI * Math.Pow(r,3);
+            string ans = "เส้นรอบวง = " + vol;
+            txtAns.Text = ans;
+        }
+    }
+}
